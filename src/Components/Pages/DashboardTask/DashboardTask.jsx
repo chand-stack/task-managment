6707e54663ea902568db087b3ad2c83dda500503
chand-stack <chand.stack@gmail.com>
2023-12-24 02:39:31 +0600

    Feat: firebase added

--- src/Components/Pages/DashboardTask/DashboardTask.jsx
+++ src/Components/Pages/DashboardTask/DashboardTask.jsx
@@ -2,12 +2,20 @@ const DashboardTask = () => {
   return (
     <div className="grid md:grid-cols-3 min-h-screen">
       <div className="border border-blue-500 h-52 md:h-screen">
-        <h1 className="text-lg md:text-2xl font-bold">Todo</h1>
+        <h1 className="text-lg md:text-2xl font-bold text-center py-3 border-y">
+          Todo
+        </h1>
       </div>
       <div className="border border-blue-500 h-52 md:h-screen">
-        <h1 className="text-lg md:text-2xl font-bold">Ongoing</h1>
+        <h1 className="text-lg md:text-2xl font-bold text-center py-3 border-y">
+          Ongoing
+        </h1>
       </div>
-      <div className="border border-blue-500 h-52 md:h-screen">Completed</div>
+      <div className="border border-blue-500 h-52 md:h-screen">
+        <h1 className="text-lg md:text-2xl font-bold text-center py-3 border-y">
+          Completed
+        </h1>
+      </div>
     </div>
   );
 };
